62fa97215ffd93aa6eaf458efa26731184e76f15
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2017-08-11 18:14:10 -0300

    Remove unused files + Add docs

--- README.md
+++ README.md
@@ -1,34 +1,12 @@
-# webdev-teste-elevador
+# Teste do Elevador
 
-## Descrição
-Construa um elevador utilizando html, javascript e css.
-Será considerado a mesma dinâmica de um elevador real e todos os casos de uso abaixo, precisam ser desenvolvidos.
+## Versões
+- **Novembro de 2016** foi a primeira versão do teste, fiz em Novembro de 2016.
+  Está intermitente e bastante desleixado;
 
-![](elevador.jpg)
+  Pode ser encontrada nessa [branch][0].
 
-## Requisitos
-- O elevador possui 5 andares;
-- Ele se move para cima ou para baixo conforme o andar é solicitado;
-- As portas abrem e permanecem abertas durante alguns segundos assim que o elevador chega no andar solicitado;
-- Se o andar onde o elevador está for solicitado novamente, as portas devem se abrir (se estiverem fechando, devem se abrir novamente);
-- O elevador precisa parar nos andares em ordem de prioridade/velocidade.
-  <br />
-  Ex: Se a direção do elevador for para cima e, ele for solicitado o 4º andar e depois no 3º, a ordem de parada deverá ser 3º andar e só então o 4º.
+- **Agosto de 2017** reescrevi o teste validando a melhora na minha abtração de
+  código e arquitetura de projetos.
 
-## Objetivos
-Essa terefa tem como objetivo avaliar:
- - Nível de conhecimento em: HTML
- - Nível de conhecimento em: CSS
- - Nível de conhecimento em: Lógica de programação
- - Nível de conhecimento em: JavaScript
-
-## Resumo
-A ideia é ter uma aplicação simples que, represente visualmente um elevador (sem necessáriamente ter um nível de realismo) e que tenha o funcionamento correto através de um bom pensamento lógico.
-
-## Prazo
-O prazo para realização dessa tarefa é de 3 dias.
-
-## Entrega
-Submeter o seu projeto no seu repositório pessoal do GiHub - (https://github.com/)[https://github.com/]
-Enviar o projeto de forma que, os arquivos não tenham nenhuma compressão, possibilitando assim a análise do código.
-Nos enviar o link.
+[0]: https://github.com/VitorLuizC/webdev-teste-elevador/tree/First
--- src/script.js
+++ src/script.js
@@ -1,259 +1,96 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
+'use strict';
 
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
+/**
+ * Obtém os elementos através de um seletor, opcionalmente de um elemento pai.
+ * @param {string} selector 
+ * @param {Element} [parent]
+ * @returns {Array.<Element>}
+ */
+const getElements = (selector, parent = document) => {
+  return [...parent.querySelectorAll(selector)];
+};
 
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
-/******/ 			return installedModules[moduleId].exports;
+/**
+ * Representação dos estados do elevator.
+ * @type {{ element: Element, open: Boolean, next: Array.<({ direction: ('top'|'bottom'), floor: Number })> }}
+ */
+const elevator = {
+  element: null,
+  open: false,
+  next: [],
+  direction: null,
+  floor: 0
+};
 
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
-/******/ 		};
+const setup = () => {
+  elevator.element = getElements('[data-id="elevator"]')[0];
 
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+  const floors = getElements('.floor-block');
 
-/******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
+  floors.forEach(floor => {
+    const number = floor.getAttribute('data-number');
+    const [ up ] = getElements('[data-action="go-up"]', floor);
+    const [ down ] = getElements('[data-action="go-down"]', floor);
 
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
+    if (up) up.addEventListener('click', () => {
+      elevator.next.push({ floor: +number, direction: 'up' });
+      updateUI();
+    });
 
+    if (down) down.addEventListener('click', () => {
+      elevator.next.push({ floor: +number, direction: 'down' });
+      updateUI();
+    });
+  });
 
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
+  updateUI();
+}
 
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
+const updateUI = () => {
+  // const shouldOpen = elevator.open && !elevator.element.classList.contains('-open');
 
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+  // if (shouldOpen) {
+  //   elevator.classList.add('-open');
+  //   setTimeout(() => {
+  //     elevator.open = false;
+  //     updateUI();
+  //   }, 1000);
+  // }
 
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ function(module, exports, __webpack_require__) {
+  
+};
 
-	"use strict";
+window.addEventListener('load', setup);
 
-	const obterElementos = __webpack_require__(1);
-	const Elevador = __webpack_require__(2);
+/**
+ * Array filter.
+ * @typedef {function(T, number, Array.<T>): boolean} Filter
+ */
 
-	var container = obterElementos(".container")[0]; 
-	var botoes = obterElementos("button", container);
+const items = ['Vitor', 'Lucas', 'Wilker'];
 
-	window.elevador = new Elevador(
-	    container,
-	    obterElementos(".andar", container),
-	    obterElementos(".elevador", container)[0]
-	);
+/**
+ * Merge **N** array filters into a single one.
+ * @example
+ * [1, 2, 3, 4, 5].filter(
+ *   mergeFilters(
+ *     n => n > 1,
+ *     n => n !== 5
+ *   )
+ * )
+ * @param {...Filter} filters
+ * @returns {Filter}
+ * @template T
+ */
+function mergeFilters(...filters) {
+  return (...args) => {
+    filters.every(filter => filter(...args));
+  };
+}
 
-	// Acoplar eventos
-	botoes.forEach(botao => {
-	    if (botao.classList.contains("subir")) {
-	        botao.addEventListener("click", e => elevador.subir(botao.parentElement));
-	    } else if (botao.classList.contains("descer")) {
-	        botao.addEventListener("click", e => elevador.descer(botao.parentElement));
-	    }
-	});
-
-
-/***/ },
-/* 1 */
-/***/ function(module, exports) {
-
-	"use strict";
-
-	/**
-	 * Obtém elementos do container pelo seletores CSS.
-	 * @param {string} seletores - Seletores CSS.
-	 * @param {Document|HTMLElement} [container = document] - DOM ou elemento de onde os seletores serão obtidos.
-	 * @returns {Array<HTMLElement>}
-	 */
-	function obterElementos(seletores, container = document) {
-	    var elementos = [];
-
-	    for (let elemento of container.querySelectorAll(seletores)) {
-	        elementos.push(elemento);
-	    }
-
-	    return elementos;
-	}
-
-	module.exports = obterElementos;
-
-
-/***/ },
-/* 2 */
-/***/ function(module, exports) {
-
-	"use strict";
-
-	/**
-	 * @param {Array<Object>} lista
-	 * @param {Function} callback
-	 * @returns {Array<Object>}
-	 */
-	function remover(lista, callback) {
-	    let index = lista.findIndex(callback);
-	    lista.splice(index, 1);
-	    return lista;
-	}
-
-	class Elevador {
-	    /**
-	     * Representação do Elevador com suas funções.
-	     * @param {HTMLElement} container - Container que contém o elevador e seus andares.
-	     * @param {Array<HTMLElement>} andares - Array com os elementos dos andares.
-	     * @param {HTMLElement} elevador - Elemento do elevador.
-	     * @constructor
-	     */
-	    constructor(container, andares, elevador) {
-	        this.container = container;
-	        this.andares = andares;
-	        this.elevador = elevador;
-
-	        // Estado do elevador, se parado, em movimento, aberto e etc
-	        this.estado = "parado";
-
-	        // Posição e orientação atuais
-	        this.atual = {
-	            andar: this.andares[0],
-	            direcao: "subir"
-	        };
-
-	        // Próximos andares
-	        this.solicitados = [];
-	    }
-
-	    /**
-	     * @param {HTMLElement} andar - andar para o qual o elevador se move.
-	     */
-	    subir(andar) {
-	        this.solicitar(andar, "subir");
-	    }
-
-	    /**
-	     * @param {HTMLElement} andar - andar para o qual o elevador se move.
-	     */
-	    descer(andar) {
-	        this.solicitar(andar, "descer");
-	    }
-
-	    /**
-	     * Move o elevador até o andar especificado.
-	     * @param {HTMLElement} andar - andar para o qual o elevador se move.
-	     * @param {string} direcao - "subir" ou "descer" são direções possíveis do elevador.
-	     */
-	    solicitar(andar, direcao) {
-	        // Não repetir itens nas solicitações
-	        if (this.solicitados.findIndex(solicitado => solicitado.andar === andar && solicitado.direcao === direcao) !== -1) {
-	            return;
-	        }
-
-	        this.solicitados.push({ andar: andar, direcao: direcao });
-
-	        if (this.atual.andar === andar && this.estado !== "movendo") {
-	            // Abro as portas do andar atual
-	            this.abrirPortas(() => {
-	                // Removo o item da fila de solicitados
-	                this.solicitados = remover(this.solicitados, _ => _.andar === andar && _.direcao === direcao);
-	                this.estado = "parado";
-	            });
-	        } else {
-	            this.estado = "movendo";
-	            this.mover(this.andares.indexOf(andar), () => {
-	                this.solicitados = remover(this.solicitados, _ => _.andar === andar && _.direcao === direcao);
-	                this.estado = "parado";
-
-	                this.abrirPortas(() => {
-	                    var proximo = this.solicitados.pop();
-	                    if (proximo instanceof Object) {
-	                        this.solicitar(proximo.andar, proximo.direcao);
-	                    }
-	                });
-	            });
-	        }
-
-	        // Reordenar a lista
-	        this.solicitados.sort((a, b) => {
-	            const index = solicitacao => this.andares.indexOf(solicitacao.andar);
-	            const subir = solicitacao => solicitacao.direcao === "subir";
-
-	            if (subir(a) && subir(b)) {
-	                if (index(a) < index(b)) {
-	                    return -1;
-	                }
-	                return 1;
-	            } else if (subir(a) && !subir(b)) {
-	                return -1;
-	            } else if (!subir(a) && subir(b)) {
-	                return 1;
-	            } else {
-	                if (index(a) > index(b)) {
-	                    return -1;
-	                }
-	                return 1;
-	            }
-	        });
-	    }
-
-	    /**
-	     * Move para o andar especificado levando em consideração o andar atual.
-	     * @param {number} andar
-	     * @param {Function} depois - Callback executado depois de mover.
-	     */
-	    mover(andar, depois) {
-	        const duracao = 3000;
-	        var altura = this.container.scrollHeight;
-	        var quantidade = this.andares.length;
-
-	        var transicao = -(altura / quantidade * andar) + "px";
-
-	        this.estado = "movendo";
-
-	        this.elevador.style.transform = `translateY(${transicao})`;
-	        this.elevador.style.transition = `transform ${duracao / 1000}s ease`;
-
-	        setTimeout(() => {
-	            this.estado = "parado";
-	            depois();
-	        }, duracao);
-	    }
-
-	    /**
-	     * Abre as portas do elevador.
-	     * @param {Function} depois - Callback executado depois da abertura das portas.
-	     */
-	    abrirPortas(depois) {
-	        const duracao = 5000;
-
-	        this.elevador.classList.add("aberto");
-	        this.estado = "aberto";
-
-	        if (this._abrirPortasTimeout) {
-	            clearTimeout(this._abrirPortasTimeout);
-	        }
-
-	        this._abrirPortasTimeout = setTimeout(() => {
-	            this.elevador.classList.remove("aberto");
-	            depois();
-	        }, duracao);
-	    }
-	}
-
-	module.exports = Elevador;
-
-
-/***/ }
-/******/ ]);
+[1, 2, 3, 4, 5].filter(
+  mergeFilters(
+    n => n > 1,
+    n => n !== 5
+  )
+)
